e1686b369e24540d1e82485904adbc57d0743d15
belibak <spam1box@tut.by>
2016-11-03 11:48:45 +0100

    fix

--- dop-del-old.sh
+++ dop-del-old.sh
@@ -18,14 +18,17 @@ for i in {1..300}
 do
 	RUNNING1=($(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" | grep \"InstanceId\" | cut -d \" -f 4))
 	RC1=${#RUNNING1[@]}
-	echo "$RC $RC1"
+	#echo "$RC $RC1"
 	if [ $RC1 -gt $RC ]
 		then
+		
+		echo "Waiting for instance run..." 
+		sleep 120
 		echo "Removing remaning instances - ${ARR[*]}"
 		aws ec2 terminate-instances --instance-ids ${ARR[*]}
 		 break
 		else
-		 echo "Waiting 10 sec... "
+		 echo "Wait until the instance starts - 10 seconds... "
 		 sleep 10
 	fi
 done;
